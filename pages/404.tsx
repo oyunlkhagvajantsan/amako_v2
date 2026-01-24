5555db5c8ab87555078989dc09155592f00aa725
oyunlkhagvajantsan <oyunlhagvajantsan@gmail.com>
2026-01-24 15:21:34 +0800

    isolation test

--- pages/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function Custom404() {
-    return (
-        <div style={{ textAlign: "center", padding: "50px" }}>
-            <h1>404 - Page Not Found</h1>
-            <p>Could not find the requested resource.</p>
-        </div>
-    );
-}
--- pages/500.tsx
+++
@@ -1,7 +0,0 @@
-export default function Custom500() {
-    return (
-        <div style={{ textAlign: "center", padding: "50px" }}>
-            <h1>500 - Server-side error occurred</h1>
-        </div>
-    );
-}
