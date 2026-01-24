2de35e26265ae83a5f86e65f9ab8dd8a1700c355
oyunlkhagvajantsan <oyunlhagvajantsan@gmail.com>
2026-01-24 14:38:39 +0800

    fallbacks

---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+export default function Custom500() {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>500 - Server-side error occurred</h1>
+        </div>
+    );
+}
